605396314dacd0ec3857f9ca978b68f704be3a26
mateosanchez777 <masaza1039@gmail.com>
2025-10-16 12:01:32 -0500

    "actualizacion"

--- DataStructures/Tree/binary_search_tree.py
+++ DataStructures/Tree/binary_search_tree.py
@@ -1,4 +1,17 @@
+import DataStructures.Tree.bst_node as bst
+
+
 def new_map():
     map = {"root":None}
     return map
 
+def insert_node(root, key, value):
+    if root is None:
+        return bst.new_node(key, value)
+    if key < root["left"]["key"]:
+        root["left"] = insert_node(root["left"], key, value)
+    elif key > root["right"]["key"]:
+        root["right"] = insert_node(root["right"], key, value)
+    else:
+        root["value"] = value
+    return root
